58c6c0d19f0f1ad0ae67eeb2b701ae449effb863
macbook-pro <krawczod@krawczod.local>
2023-03-22 09:44:03 +0100

    comments added

--- app.py
+++ app.py
@@ -4,62 +4,84 @@ import database
 app = Flask(__name__)
 app.secret_key = "secretkey"
 
+# Create a database object to interact with the database
 db = database.Db()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     error = None
     username = None
+
+    # Set the logged_in key in the session object to None
     session['logged_in'] = None
+
     if request.method == 'POST':
+        # Get the email and password values from the form
         email    = request.form['email']
         password = request.form['password']
 
+        # Get the email, password, and username values from the database based on the provided email
         db_email = db.get('email', 'email', email)
         db_password = db.get('password','password', password)
         username = db.get('username', 'email', email)
 
+        # If the email or password is incorrect, set an error message
         if email != db_email or password != db_password:
             error = 'Wrong email address or password. Try arain!'
         else:
+            # Otherwise, set the logged_in key in the session object to True and redirect to the user page
             session['logged_in'] = True
             return redirect(url_for('user', username=username))
         
+    # Render the home page template with the error message, if any
     return render_template('home.html', error=error)
+
 
 @app.route('/reqistration', methods=['GET', 'POST'])
 def registration():
     error= None
+    
     if request.method == 'POST':
+        # Get the username, email, password, and re-typed password values from the form
         username    = request.form['username']
         email       = request.form['email']
         password    = request.form['password']
         re_password = request.form['re-password']
         
+        # Create a tuple with the user data
         user_data = (username, email, password)
 
+        # Check if the provided email already exists in the database
         if email == db.get('email', 'email', email):
             error = 'This email adress already exists.'
+        # Check if the provided password and re-typed password match
         elif password != re_password:
             error = 'Password is not the same. Try again.'
         else:
+            # If the email and password are valid, insert the user data into the database and redirect to the home page
             msg='Account is created and ready to use. You can log in now.'
             db.insert(user_data)
             return redirect(url_for('home'))
             
+    # Render the registration page template with the error message, if any
     return render_template('registration.html', error=error)
+
 
 @app.route('/user/<username>')
 def user(username):
+    # If the user is not logged in, redirect to the home page
     if not session['logged_in']:
         return redirect(url_for('home'))
     else:
+        # Otherwise, render the user page template with the provided username
         return render_template('user.html', username = username)
 
 @app.route('/logout')
 def logout():
+    # Set the logged_in key in the session object to None and redirect to the home page
     session['logged_in'] = None
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
+    # Run the Flask app
     app.run()
--- database.py
+++ database.py
@@ -1,18 +1,22 @@
 import psycopg2
 
 class Db():
+    # Constructor function that initializes the database connection parameters and creates a table if it does not already exist
     def __init__(selfo, hostname, database, username, port, password):
-
+        # Set the database connection parameters
         self.hostname = hostname
         self.database = database
         self.username = username
         self.port     = port
         self.password = password
 
+        # Get a database connection and create the users table
         self.get_db_connection()
         self.create_table()
 
+    # Function that gets a database connection
     def get_db_connection(self):
+        # Connect to the database with the specified parameters
         conn = psycopg2.connect(
                 host   = self.hostname,
                 dbname = self.database,
@@ -20,9 +24,12 @@ class Db():
                 port   = self.port,
                 password = self.password)
         cur = conn.cursor()
+        # Return the connection and cursor objects
         return conn, cur
 
+    # Function that creates a users table if it does not already exist
     def create_table(self):
+        # Get a database connection and create the users table if it does not exist
         conn, cur = self.get_db_connection()
         command = """create table if not exists users(
                         id serial primary key,
@@ -36,7 +43,9 @@ class Db():
         cur.close()
         conn.close()
 
+    # Function that inserts a new user into the users table
     def insert(self, values):
+        # Get a database connection and insert the values into the users table
         conn, cur = self.get_db_connection()
         command = '''
             insert into users(username, email, password)
@@ -49,7 +58,9 @@ class Db():
         cur.close()
         conn.close()
 
+    # Function that gets a single value from the users table based on a specified column and value
     def get(self, column1, column2, value):
+        # Get a database connection and select the value from the users table based on the specified column and value
         conn, cur = self.get_db_connection()
         command = f'''
             select {column1} from users where {column2}=%s
@@ -57,8 +68,8 @@ class Db():
         
         cur.execute(command, (value,))
         data = cur.fetchone()
+        # If data is found, close the connection and return the first value in the data tuple
         if data:
             cur.close()
             conn.close()
             return data[0] 
-
